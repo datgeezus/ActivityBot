25b0fe9629c662654e8dcc32febc63c5a50953a3
Jesus Sanchez <jesus.san.sev@gmail.com>
2015-07-19 20:48:05 -0500

    CNY check

--- main.c
+++ main.c
@@ -1,33 +1,58 @@
 #include <stdint.h>
 #include "simpletools.h"
+#include "abdrive.h"   
 
-#define CYCLES_PER_MS   1000
-#define HB_MAX          CYCLES_PER_MS * 10
-#define HB_PIN          26
+#define CYCLES_PER_MS       1000
+#define HB_MAX              CYCLES_PER_MS * 10
+#define PIN_HB              26
+#define PIN_RIGHT_ENCODER   15
+#define PIN_LEFT_ENCODER    14
+#define PIN_CNY_1           13
+#define PIN_CNY_0           12
+#define IN_MAX              8
+#define COGS_MAX            8
 
-typedef enum State
+typedef enum
 {
     ST_IDLE,
-    ST_AVOID,
-};
+    ST_INPUTS,
+    ST_MOVE
+} State;
 
-typedef void(*ProcessCB)(void* data);
+typedef enum
+{
+    MV_FORWARDS,
+    MV_BACKWARDS
+} Move;
 
-void Process_Avoid(void* data);
 void Process_Idle(void* data);
+void Process_CheckInputs(void* data);
+void Process_Move(void* data);
+void Move_Toggle(void* data);
 void doHeartBeat(void);
 
 uint32_t heartBeatCount = 0U;
 uint8_t heartBeatLast = 0U;
 
-State state = ST_IDLE;
+int* cog[COGS_MAX];
+uint8_t inPins[] = {8, 9, 10, 11, 12, 13, 14, 15};
+volatile uint8_t inputs[IN_MAX];
 
-ProcessCB Process[] = {Process_Idle, Process_Avoid};
+volatile State state = ST_IDLE;
+volatile Move lastMove = MV_FORWARDS;
+
+typedef void(*ProcessCB)(void* data);
+ProcessCB Process[] = {Process_Idle, Process_CheckInputs, Process_Move};
+
 void(*Toggle[])(int pin) = {high, low};
 
 int main(void)
 {
     state = ST_IDLE;
+
+    cog[0] = cog_run(Process_CheckInputs, 128);
+    cog[1] = cog_run(Process_Move, 128);
+    cog[2] = cog_run(Move_Toggle, 128);
 
     while(1)
     {
@@ -40,13 +65,45 @@ int main(void)
 
 void Process_Idle(void* data)
 {
-    pause(10000);
-    state = ST_AVOID;
+    pause(8000);
+    drive_speed(32, 32);
+    state = ST_INPUTS;
 }
 
-void Process_Avoid(void* data)
+void Process_CheckInputs(void* data)
 {
-    return;
+    uint8_t i;
+    for(i = 0; i < IN_MAX; ++i)
+    {
+        inputs[i] = input(inPins[i]);
+    }
+
+    state = ST_MOVE;
+}
+
+void Process_Move(void* data)
+{
+    uint8_t lineSensed = !(inputs[PIN_CNY_0] && inputs[PIN_CNY_1]);
+    if(lineSensed)
+    {
+        Move_Toggle(0);
+    }
+    
+    state = ST_INPUTS;
+}
+
+void Move_Toggle(void* data)
+{
+    if(lastMove == MV_FORWARDS)
+    {
+        drive_speed(64, 64);
+        lastMove == MV_FORWARDS;
+    }
+    else
+    {
+        drive_speed(-64, -64);
+        lastMove = MV_BACKWARDS;
+    }
 }
 
 void doHeartBeat(void)
@@ -56,7 +113,7 @@ void doHeartBeat(void)
     {
         // toogle
         heartBeatLast ^= 1U;
-        Toggle[heartBeatLast](HB_PIN);
+        Toggle[heartBeatLast](PIN_HB);
         heartBeatCount = 0;
     }
 }
