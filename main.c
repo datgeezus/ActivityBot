91855b50565258fde746d48873f8570e37dc1710
Jesus Sanchez <jesus.san.sev@gmail.com>
2015-07-19 22:06:15 -0500

    QRD check

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include "simpletools.h"
 #include "abdrive.h"   
 
-#define CYCLES_PER_MS       1000
+#define CYCLES_PER_MS       100
 #define HB_MAX              CYCLES_PER_MS * 10
 #define PIN_HB              26
-#define PIN_RIGHT_ENCODER   15
-#define PIN_LEFT_ENCODER    14
-#define PIN_CNY_1           13
-#define PIN_CNY_0           12
+#define PIN_SENSE           27
+
+#define PIN_RIGHT_ENCODER   7 // 15
+#define PIN_LEFT_ENCODER    6 // 14
+#define PIN_CNY_1           5 // 13
+#define PIN_CNY_0           4 // 12
 #define IN_MAX              8
+
 #define COGS_MAX            8
 
 typedef enum
@@ -24,6 +27,7 @@ typedef enum
     MV_FORWARDS,
     MV_BACKWARDS
 } Move;
+
 
 void Process_Idle(void* data);
 void Process_CheckInputs(void* data);
@@ -44,19 +48,19 @@ volatile Move lastMove = MV_FORWARDS;
 typedef void(*ProcessCB)(void* data);
 ProcessCB Process[] = {Process_Idle, Process_CheckInputs, Process_Move};
 
-void(*Toggle[])(int pin) = {high, low};
+void(*Toggle[])(int pin) = {low, high};
 
 int main(void)
 {
     state = ST_IDLE;
 
-    cog[0] = cog_run(Process_CheckInputs, 128);
-    cog[1] = cog_run(Process_Move, 128);
-    cog[2] = cog_run(Move_Toggle, 128);
+    //cog[0] = cog_run(Process_CheckInputs, 128);
+    //cog[1] = cog_run(Process_Move, 128);
+    //cog[2] = cog_run(Move_Toggle, 128);
 
     while(1)
     {
-        Process[state];
+        Process[state](0);
 
         doHeartBeat();
     }
@@ -66,7 +70,8 @@ int main(void)
 void Process_Idle(void* data)
 {
     pause(8000);
-    drive_speed(32, 32);
+    //drive_speed(64, 64);
+    high(PIN_SENSE);
     state = ST_INPUTS;
 }
 
@@ -77,13 +82,14 @@ void Process_CheckInputs(void* data)
     {
         inputs[i] = input(inPins[i]);
     }
-
+    
+    Toggle[inputs[PIN_CNY_0]](PIN_SENSE);
     state = ST_MOVE;
 }
 
 void Process_Move(void* data)
 {
-    uint8_t lineSensed = !(inputs[PIN_CNY_0] && inputs[PIN_CNY_1]);
+    uint8_t lineSensed = !(inputs[PIN_CNY_0]) && !(inputs[PIN_CNY_1]);
     if(lineSensed)
     {
         Move_Toggle(0);
@@ -96,12 +102,14 @@ void Move_Toggle(void* data)
 {
     if(lastMove == MV_FORWARDS)
     {
-        drive_speed(64, 64);
+        //drive_speed(64, 64);
+        //Toggle[0](PIN_SENSE);
         lastMove == MV_FORWARDS;
     }
     else
     {
-        drive_speed(-64, -64);
+        //drive_speed(-64, -64);
+        //Toggle[1](PIN_SENSE);
         lastMove = MV_BACKWARDS;
     }
 }
